790ba525e3a32f29b49b2fa2aedfe9e06534c548
Alexey <gevorkyan.alexey@gmail.com>
2020-01-21 19:52:22 +0300

    first commit

---
+++ NetClient.js
@@ -0,0 +1,125 @@
+const net = require('net');
+const events = require('events');
+
+class NetClient extends events {
+  constructor(props = {}) {
+    super();
+    this._eol = '\n';
+    this._taskIdField = props.taskIdField || '_taskId';
+    this.host = props.host || '127.0.0.1';
+    this.port = props.port || 80;
+    this._dataField = 'payload';
+    this._eventKey = 'taskId';
+    this._taskId = 0;
+    this._taskIdLimit = 65536;
+
+    this._initConnection();
+  }
+
+  _initConnection() {
+    this.tasksSet = new Set();
+    // this.messagesMap = new Map();
+    const options = {
+      host: this.host,
+      port: this.port,
+      rejectUnauthorized: true,
+      requestCert: true
+    };
+    const socket = net.connect(options);
+    this.socket = socket;
+    // process.stdin.pipe(socket);
+    // this.socket.on('secureConnect', () => {console.log('successfully connected')});
+    this.socket.on('data', this._parseDataFromServer.bind(this));
+    // this.socket.on('error', err => console.error('Error on client', err));
+    this.socket.on('close', async () => {
+      this._flushAllTasks();
+      await this._delay(5 * 1000);
+      this._initConnection();
+    });
+  }
+
+  _flushAllTasks() {
+    for (let taskId of this.tasksSet.keys()) {
+      this.tasksSet.delete(taskId);
+      this._emitTaskComplete(taskId, 'Task flushed');
+    }
+  }
+
+  _parseDataFromServer(buff) {
+    // console.log('getCipher()', socket.getCipher());
+    let str = buff.toString();
+    let obj = {};
+    let jsons = str.split(this._eol);
+    for (let i = 0; i < jsons.length; i++) {
+      let json = jsons[i];
+      if (!json) continue;
+      try {
+        obj = JSON.parse(json);
+        let taskId = this._getTaskIdFromObj(obj);
+        this._deleteTaskIdFromObj(obj);
+        this._emitTaskComplete(taskId, obj);
+      } catch (e) {
+        console.log('e', e);
+        // this.sendErrorMessage(socket, this._getTaskIdFromObj(obj), e);
+      }
+    }
+    // console.log('this', this);
+  }
+
+  async _sendDataMessage(data) {
+    let obj = {
+      [this._dataField]: data
+    };
+    let {message, taskId} = this._formatDataToSend(obj);
+    this.socket.write(message);
+    const result = await this._createTaskListener(taskId);
+    return result;
+  }
+
+  _formatDataToSend(data) {
+    let objToSend = data || {};
+    const taskId = this._getNewTaskId();
+    objToSend[this._taskIdField] = taskId;
+    return {message: `${JSON.stringify(objToSend)}${this._eol}`, taskId};
+  }
+  _getNewTaskId() {
+    return ++this._taskId % this._taskIdLimit;
+  }
+  _getTaskIdFromObj(obj) {
+    if (!obj) throw new Error('Obj is required');
+    return obj[this._taskIdField];
+  }
+  async _createTaskListener(taskId) {
+    return await Promise.race([
+      new Promise(res => {
+        this.tasksSet.add(taskId);
+        this.once(`${this._eventKey}:${taskId}`, res)
+      }),
+      this._delay(20 * 1000, true)
+    ])
+  }
+
+  _emitTaskComplete(taskId, result) {
+    this.tasksSet.delete(taskId);
+    this.emit(`${this._eventKey}:${taskId}`, result);
+  }
+
+  async _delay(ms, throwTimeoutError, timeoutMessage = 'My timeout error') {
+    return await new Promise((res, rej) =>
+      (throwTimeoutError) ? setTimeout(rej, ms, timeoutMessage) : setTimeout(res, ms)
+    );
+  }
+
+  _deleteTaskIdFromObj(obj) {
+    if (!obj || typeof obj !== 'object') return;
+    delete obj[this._taskIdField];
+  }
+
+  async sendData(data) {
+    // console.log('data', data);
+    if (!data || typeof data !== 'object') throw new Error('Data is required');
+    return await this._sendDataMessage(data);
+  }
+}
+
+module.exports = NetClient;
---
+++ NetServer.js
@@ -0,0 +1,95 @@
+const net = require('net');
+
+class NetServer extends net.Server {
+  constructor(...props) {
+    super(...props);
+    this._eol = '\n';
+    this._taskIdField = props.taskIdField ||  '_taskId';
+    this._methodField = 'method';
+    this._dataField = 'payload';
+    this._methodParamsField = 'data';
+    this._routeMap = new Map();
+    this.on('connection', (socket) => {
+      this.listeningFunc(socket);
+    })
+    // this.on('error', err => console.error('Error on server', err));
+  }
+  listeningFunc(socket) {
+    socket.on('data', (data) => this._parseDataFromClient(socket, data));
+  }
+
+  async _parseDataFromClient(socket, buff) {
+    let str = buff.toString();
+    let obj = {};
+    let jsons = str.split(this._eol);
+    for (let i = 0; i < jsons.length; i++) {
+      try {
+        let json = jsons[i];
+        if (!json) continue;
+        obj = JSON.parse(json);
+        if (!obj[this._taskIdField]) return this._badRequest(socket);
+        const taskId = this._getTaskIdFromObj(obj);
+        if (!obj[this._dataField]) return this._badRequest(socket, taskId, `${this._dataField} is required`);
+        const payload = obj[this._dataField];
+        const routePath = payload[this._methodField];
+        let routeFunc = this._routeMap.get(routePath);
+        if (!routeFunc) return this._badRequest(socket, taskId, `Not found "${routePath}" in routes`);
+        let data = payload[this._methodParamsField];
+        try {
+          let result = await routeFunc(data);
+          this._sendDataMessage(socket, taskId, result);
+        } catch (e) {
+          this._sendErrorMessage(socket, taskId, e);
+        }
+      } catch (e) {
+        this._sendErrorMessage(socket, this._getTaskIdFromObj(obj), e);
+      }
+    }
+  }
+
+  _getTaskIdFromObj(obj) {
+    if (!obj || typeof obj !== 'object') throw new Error('Obj must be and object');
+    return obj[this._taskIdField];
+  }
+
+
+  _badRequest(socket, taskId, text = 'badRequest') {
+    this._sendErrorMessage(socket, taskId, text);
+  }
+
+  _sendDataMessage(socket, taskId, text = 'success') {
+    let obj = {
+      data: text
+    };
+    let message = this._formatDataToSend(taskId, obj);
+    socket.write(message);
+  }
+
+  _sendErrorMessage(socket, taskId, text = 'unknown error') {
+    console.error('error', text);
+    if (text.message) text = text.message;
+    let obj = {
+      error: text
+    };
+    let message = this._formatDataToSend(taskId, obj);
+    socket.write(message);
+  }
+
+  _formatDataToSend(taskId, data) {
+    let objToSend = data || {};
+    if (Number.isInteger(taskId)) objToSend._taskId = taskId;
+    return `${JSON.stringify(objToSend)}${this._eol}`;
+  }
+
+  addRoute(route, func) {
+    if (this._routeMap.has(route)) throw new Error(`Server already have route "${route}"`);
+    this._routeMap.set(route, func);
+  }
+
+  deleteRoute(route) {
+    if (!route) throw new Error('Route is required');
+    return this._routeMap.delete(route);
+  }
+}
+
+module.exports = NetServer;
